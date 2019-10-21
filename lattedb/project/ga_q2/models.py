b850afcb490fed925090df15130a9573a95fcb47
Christopher Koerber <christopher@ckoerber.com>
2019-10-21 15:52:46 -0700

    Added ga_q2 project for one-to-all status

---
+++ lattedb/project/ga_q2/models.py
@@ -0,0 +1,46 @@
+"""Models of ga_q2
+"""
+
+# Note: if you want your models to use espressodb features, they must inherit from Base
+
+from django.db import models
+from espressodb.base.models import Base
+from lattedb.propagator.models import OneToAll
+
+
+class OneToAllStatus(Base):
+    """Status of the one-to-all propagator files
+    """
+
+    propagator = models.ForeignKey(
+        OneToAll,
+        on_delete=models.CASCADE,
+        help_text="The one-to-all propagator describing properties of this file.",
+    )
+    machine = models.CharField(
+        max_length=120, help_text="The machine hosting the file."
+    )
+    file_location = models.TextField(help_text="The path to the file.")
+    file_size = models.CharField(
+        max_length=120, help_text="The size of the file in Bytes."
+    )
+    mtime = models.DateTimeField(
+        null=True, help_text="The last time the file was modified."
+    )
+    exists = models.BooleanField(
+        default=False, help_text="File exists at `file_location` on `machine`"
+    )
+    short_tag = models.CharField(
+        max_length=20, help_text="Ensemble short quantifier like `a09m130`."
+    )
+    src_set = models.CharField(
+        max_length=40, help_text="The source group the file belongs to. E.g., `0-8`"
+    )
+
+    class Meta:
+        unique_together = ["propagator", "machine", "file_location", "src_set"]
+
+    def check_consistency(self, data):
+        """Checks if the ensemble of the propagator is quantified properly
+        """
+        assert data["propagator"].gaugeconfig.short_tag == data["short_tag"]
