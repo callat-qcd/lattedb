4ad29d62138c759eee917293c2a0fe7eb8fc6f26
Christopher Koerber <christopher@ckoerber.com>
2019-10-30 17:38:37 -0700

    Added new fhcompare models to models.__init__

--- lattedb/project/fhcompare/models/__init__.py
+++ lattedb/project/fhcompare/models/__init__.py
@@ -1,3 +1,5 @@
 from lattedb.project.fhcompare.models.analysis import Fhcompare
 from lattedb.project.fhcompare.models.data import SourceAvg2pt
+from lattedb.project.fhcompare.models.data import SourceAvgSeq3pt
+from lattedb.project.fhcompare.models.data import SourceAvgFH3pt
 from lattedb.project.fhcompare.models.user_tables import Jason0
