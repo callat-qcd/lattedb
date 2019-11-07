257fec68df77e5842047c6adc47750c56456adca
Christopher Koerber <christopher@ckoerber.com>
2019-11-07 11:17:20 -0800

    Made formfactor file tables abstract to generalize for other models

--- lattedb/project/formfac/models/__init__.py
+++ lattedb/project/formfac/models/__init__.py
@@ -1,6 +1,8 @@
 """Imports all models from the formfac.models submodules
 """
 
-from lattedb.project.formfac.models.data import ConcatenatedFormFactor4DFile
-from lattedb.project.formfac.models.data import DiskConcatenatedFormFactor4DFile
-from lattedb.project.formfac.models.data import TapeConcatenatedFormFactor4DFile
+from lattedb.project.formfac.models.data.concatenated_ff4d import (
+    ConcatenatedFormFactor4DFile,
+    DiskConcatenatedFormFactor4DFile,
+    TapeConcatenatedFormFactor4DFile,
+)
---
+++ lattedb/project/formfac/models/data/concatenated_ff4d.py
@@ -0,0 +1,56 @@
+"""Models of concatenated form factor 4D files.
+"""
+from django.db import models
+from lattedb.project.formfac.models.data import (
+    AbstractFormFactor4DFile,
+    PhysicalFormFactor4DFile,
+)
+
+
+class ConcatenatedFormFactor4DFile(AbstractFormFactor4DFile):
+    """Table storing information about form factor files.
+
+    This is not the "physical" file information but rather the meta info.
+    """
+
+    configuration_range = models.CharField(
+        max_length=100,
+        help_text="Range of configuration in this file. E.g., `500-1745`.",
+    )
+
+    class Meta:  # pylint: disable=C0111, R0903
+        unique_together = [
+            "ensemble",
+            "stream",
+            "configuration_range",
+            "source_set",
+            "current",
+            "state",
+            "parity",
+            "flavor",
+            "spin",
+        ]
+
+
+class TapeConcatenatedFormFactor4DFile(PhysicalFormFactor4DFile):
+    """Table associates Form Factor file meta information with a physical file on tape.
+    """
+
+    file = models.ForeignKey(
+        ConcatenatedFormFactor4DFile,
+        on_delete=models.CASCADE,
+        related_name="tape",
+        help_text="The file meta information.",
+    )
+
+
+class DiskConcatenatedFormFactor4DFile(PhysicalFormFactor4DFile):
+    """Table associates Form Factor file meta information with a physical file on disk.
+    """
+
+    file = models.ForeignKey(
+        ConcatenatedFormFactor4DFile,
+        on_delete=models.CASCADE,
+        related_name="disk",
+        help_text="The file meta information.",
+    )
