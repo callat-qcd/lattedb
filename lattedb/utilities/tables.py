0f1601fadb9a90d7c12d6971fba439695f37299c
Christopher Koerber <christopher@ckoerber.com>
2019-11-07 16:40:13 -0800

    More query options

--- lattedb/utilities/tables.py
+++ lattedb/utilities/tables.py
@@ -11,20 +11,50 @@ def to_table(df: DataFrame, id_name: str) -> str:  # pylint: disable=C0103
         table_id=id_name,
         index=False,
     )
+    table = table.replace(
+        "</table>", f"<tfoot><th>{'</th><th>'.join(df.columns)}</th></tfoot></table>"
+    )
+
     script = """<script>
     $(document).ready(function () {{
+        // Define sum function
         $.fn.dataTable.Api.register( 'sum()', function () {{
             var sum = 0;
             var total = 0;
-
             for ( var i=0, ien=this.length ; i<ien ; i++ ) {{
                 if (this[i] == 'True') {{ sum += 1;}}
                 total += 1;
             }}
-
             return {{exists: sum, total: total}};
         }});
-        var table = $('#{id_name}').DataTable();
+        // Add footer with serach
+        $('#{id_name} tfoot th').each( function () {{
+            var title = $(this).text();
+            $(this).html( '<input type="text" placeholder="' + title + '"/>' );
+        }});
+        // Create table
+        var table = $('#{id_name}').DataTable({{
+            "lengthMenu": [ [10, 25, 50, -1], [10, 25, 50, "All"] ],
+            "pageLength": 50,
+            initComplete: function () {{
+              var r = $('#{id_name} tfoot tr');
+              r.find('th').each(function(){{
+                $(this).css('padding', 8);
+              }});
+              $('#{id_name} thead').append(r);
+              $('#search_0').css('text-align', 'center');
+            }},
+        }});
+        // Apply the search
+        table.columns().every( function () {{
+            var that = this;
+            $( 'input', this.footer() ).on( 'keyup change clear', function () {{
+                if ( that.search() !== this.value ) {{
+                    that.search( this.value ).draw();
+                }}
+            }});
+        }});
+        // Update progress bar
         table.on( 'search.dt', function () {{
             var info = table.column(9, {{ filter : 'applied'}} ).data().sum();
             if (info.total > 0){{
@@ -37,6 +67,7 @@ def to_table(df: DataFrame, id_name: str) -> str:  # pylint: disable=C0103
                 $("#progress-error").attr("aria-valuenow", 0).css("width", "0%");
             }}
         }});
+
     }});
     </script>""".format(
         id_name=id_name
