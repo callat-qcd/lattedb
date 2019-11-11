35c48ec81e782db88fd9f8a9ce7822f3d2fb9a43
Christopher Koerber <christopher@ckoerber.com>
2019-11-11 12:57:20 -0800

    Added class methods to check status of dependencies

--- lattedb/project/formfac/models/data/tsliced_ff4d.py
+++ lattedb/project/formfac/models/data/tsliced_ff4d.py
@@ -1,6 +1,9 @@
 """Models of time sliced form factor 4D files.
 """
+from typing import List
+
 from django.db import models
+
 from lattedb.project.formfac.models.data import (
     AbstractFormFactor4DFile,
     PhysicalFormFactor4DFile,
@@ -55,3 +58,30 @@ class DiskTSlicedFormFactor4DFile(PhysicalFormFactor4DFile):
         related_name="disk",
         help_text="The file meta information.",
     )
+
+    @property
+    def dependency_exists(self) -> bool:
+        """Check wether dependent form factor 4D file exists on disk
+        """
+        return self.file.dependency.disk.exists  # pylint: disable=E1101
+
+    @classmethod
+    def get_missing_files(cls) -> List["DiskTSlicedFormFactor4DFile"]:
+        """Get all DiskTSlicedFormFactor4DFile which don't exist.
+        """
+        cls.objects.filter(exists=False)
+
+    @classmethod
+    def get_files_with_missing_dependencies(cls) -> List["DiskTSlicedFormFactor4DFile"]:
+        """Get all DiskTSlicedFormFactor4DFile where the dependency does not exist.
+
+        Dependency does not exists is defined by:
+            1. No dependency file is in the db,
+            2. No file status is associated with the dependency file,
+            3. The file status of the dependency file is does not exist.
+        """
+        return cls.objects.filter(
+            models.Q(file__dependency__isnull=True)
+            | models.Q(file__dependency__disk__isnull=True)
+            | models.Q(file__dependency__disk__exists=False)
+        )
