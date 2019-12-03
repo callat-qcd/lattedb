5c8200506f4ca898426c032961c7f2f233359c61
Christopher Koerber <christopher@ckoerber.com>
2019-12-02 16:05:24 -0800

    Added current tests

--- lattedb/current/tests.py
+++ lattedb/current/tests.py
@@ -4,25 +4,33 @@ Unittests for the current module
 
 from django.test import TestCase
 
-from lattedb.utilities.tests import ObjectParser
+from lattedb.utilities.tests import ObjectParser, BaseTest
 
 from lattedb.current.models import Local
 
 
-class LocalTestCase(ObjectParser, TestCase):
+class LocalParser(ObjectParser):
     model = Local
-    tree = None
-    parameters = {
+    _tree = None
+    _parameters = {
         "diracstruct": "gamma_5",
         "momentum": "0",
         "description": "pseudoscalar",
     }
 
 
+class LocalTestCase(LocalParser, BaseTest, TestCase):
+    ""
+
+
 from lattedb.current.models import Local4D
 
 
-class Local4DTestCase(ObjectParser, TestCase):
+class Local4DParser(ObjectParser, TestCase):
     model = Local4D
-    tree = None
-    parameters = {"diracstruct": "gamma_3*gamma_5", "description": "A3"}
+    _tree = None
+    _parameters = {"diracstruct": "gamma_3*gamma_5", "description": "A3"}
+
+
+class Local4DTestCase(Local4DParser, TestCase):
+    ""
