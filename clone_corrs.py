50af23cfef3dc1784ecc9d6c15002b2d6a74b9a2
ckoerber <software@ckoerber.com>
2020-04-08 12:20:39 +0200

    Added updating db script

--- clone_corrs.py
+++ clone_corrs.py
@@ -11,6 +11,8 @@ from argparse import ArgumentParser
 
 from collections import OrderedDict
 
+from tqdm import tqdm
+
 from lattedb.config.settings import DATABASES
 from lattedb.project.formfac.models.data.correlator import (
     CorrelatorMeta,
@@ -23,6 +25,7 @@ from lattedb.project.formfac.models.data.correlator import (
 
 from django.contrib.auth.models import User
 from django.db import transaction
+from django.db.models import ManyToManyRel, ManyToOneRel
 
 PARSER = ArgumentParser(
     description="Script allows to export all data to and from default"
@@ -34,6 +37,12 @@ PARSER.add_argument(
     dest="new",
     action="store_true",
     help="Populate new local database from default. Local must be empty.",
+)
+PARSER.add_argument(
+    "-u",
+    dest="update",
+    action="store_true",
+    help="Populate default database from local database.",
 )
 
 
@@ -66,6 +75,64 @@ def create_new_local_from_default():
             Model.objects.using("local").bulk_create(objects)
 
 
+def assert_queryset_equal(qs1, qs2):
+    """Compares two querysets and checks if all rows and columns are equal
+
+    Ignores ManyToManyRel, ManyToOneRel and last_modified
+    """
+    # Check if same count
+    assert qs1.count() == qs2.count()
+    # Check if same ids
+    assert sorted(qs1.values_list("id")) == sorted(qs2.values_list("id"))
+    # Check if all columns are the same
+    for obj1, obj2 in tqdm(list(zip(qs1.order_by("-id"), qs2.order_by("-id")))):
+        for field in obj1._meta.get_fields():
+            if (
+                isinstance(field, (ManyToManyRel, ManyToOneRel))
+                or field.name == "last_modified"
+            ):
+                continue
+            if not getattr(obj1, field.name) == getattr(obj2, field.name):
+                raise AssertionError(f"{obj1} != {obj2}\nfield: {field}")
+
+
+def update_default_from_local():
+    """
+    """
+    new_local_objects = OrderedDict()
+    # First check that all the data present on default is also present on local
+    print("Checking consistency")
+    for Model in [User, CorrelatorMeta, DiskCorrelatorH5Dset, TapeCorrelatorH5Dset]:
+        print("\t*", Model)
+        # Store data in default db
+        default_objects = Model.objects.all()
+        local_objects = Model.objects.using("local").filter(
+            pk__in=list(default_objects.values_list("pk", flat=True))
+        )
+        assert_queryset_equal(default_objects, local_objects)
+        new_local_objects[Model] = (
+            Model.objects.using("local").all().difference(local_objects)
+        )
+
+    print("Now updating default with new objects")
+    print(
+        "\n".join(
+            [f"\t* {key}: {val.count()}" for key, val in new_local_objects.items()]
+        )
+    )
+    go_on = input("Continue (y/N):")
+    if not go_on.lower() == "y":
+        raise KeyboardInterrupt()
+
+    # Insert data atomic in new db
+    with transaction.atomic():
+        for Model, objects in new_local_objects.items():
+            # Can use bulk create because objects don't exist
+            Model.objects.bulk_create(objects)
+
+    print("Done")
+
+
 def main():
     """Runs the copy scripts
     """
@@ -79,6 +146,15 @@ def main():
             )
         )
         create_new_local_from_default()
+        return
+
+    if args.update:
+        print(
+            "Updating default database {default} from local database {local}.".format(
+                local=DATABASES["local"]["NAME"], default=DATABASES["default"]["NAME"]
+            )
+        )
+        update_default_from_local()
 
 
 if __name__ == "__main__":
