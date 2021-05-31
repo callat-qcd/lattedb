08b02c8da3b1786bd3170938263c846df5dda9ce
Christopher Koerber <software@ckoerber.com>
2021-05-31 16:40:32 +0200

    Updated all app names to be properly loaded for Django 3.2
    This should be backwards compatible (unittests still run)

--- lattedb/fermionaction/apps.py
+++ lattedb/fermionaction/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class ActionConfig(AppConfig):
-    name = 'fermionaction'
+    name = 'lattedb.fermionaction'
--- lattedb/quarksmear/apps.py
+++ lattedb/quarksmear/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class QuarkSmearConfig(AppConfig):
-    name = "quarkmear"
+    name = "lattedb.quarksmear"
